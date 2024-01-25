aa95420fdb9fc7b5624d9d115f30bad4550adde7
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-01-25 19:09:26 +0900

    update: refactor the route for user, as a user controller been created.

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/usersController');
 
 // Define a route
-router.get('/', (req, res) => {
-  // this get executed when user visit http://localhost:8000/user
-  res.send('this is a user route');
-});
 
-router.get('/101', (req, res) => {
-  // this get executed when user visit http://localhost:8000/user/101
-  res.send('this is user 101 route');
-});
+// this get executed when user visit http://localhost:8000/user
+router.get('/', userController.getUser);
+
+// this get executed when user visit http://localhost:8000/:userId
+router.get('/:userId', userController.getUserById);
 
 // export the router module, that server.js file can use
 module.exports = router;
